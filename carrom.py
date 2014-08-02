3d04abe05e3edfbff4c68edabb8c07e116d2f30f
aseemraj <aseem.mathswiz@gmail.com>
2014-08-02 20:06:59 +0530

    gotiyan and pillak

--- carrom.py
+++ carrom.py
@@ -1,121 +1,39 @@
 import pygame, random
 # from pygame.locals import *
+from definitions import *
+from math import *
+# from numpy import *
+
 
 def drawCorners():
-    pygame.draw.circle(screen, BLACK, [border, border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [wid-border, border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [border, wid-border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [wid-border, wid-border], pocketrad)
+    pygame.draw.circle(screen, BLACK, [2*border/3, 2*border/3], pocketrad)
+    pygame.draw.circle(screen, BLACK, [wid-2*border/3, 2*border/3], pocketrad)
+    pygame.draw.circle(screen, BLACK, [2*border/3, wid-2*border/3], pocketrad)
+    pygame.draw.circle(screen, BLACK, [wid-2*border/3, wid-2*border/3], pocketrad)
     return
 
-def drawGotiW(screen, x, y):
-    pygame.draw.circle(screen, WHITE, [x, y], gotirad)
-    pygame.draw.circle(screen, BLACK, [x, y], gotirad, 1)
+def drawBorder():
+    for i in range(0, border/2, 3):
+        if i%2==0:
+            pygame.draw.rect(screen, LBROWN, [i, i, wid-2*i, wid-2*i], 3)
+        else:
+            pygame.draw.rect(screen, DBROWN, [i, i, wid-2*i, wid-2*i], 3)
+
     return
-
-def drawGotiB(screen, x, y):
-    pygame.draw.circle(screen, (30, 30, 30), [x, y], gotirad)
-    return
-
-class Goti(pygame.sprite.Sprite):
-    """
-    This class represents the goti.
-    It derives from the "Sprite" class in Pygame.
-    """
-    def __init__(self, color, width, height):
-        """ Constructor. Pass in the color of the block,
-        and its x and y position. """
- 
-        # Call the parent class (Sprite) constructor
-        pygame.sprite.Sprite.__init__(self)
- 
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
-        self.image.fill(WOOD)
-        pygame.draw.ellipse(self.image, color, [0, 0, width, height])
-        # Fetch the rectangle object that has the dimensions of the image
-        # image.
-        # Update the position of this object by setting the values
-        # of rect.x and rect.y
-        self.rect = self.image.get_rect()
-        self.velx = 2
-        self.vely = 1
-        self.collided = False
-    
-    def reset_pos(self):
-        """ Reset position to the top of the screen, at a random x location.
-        Called by update() or the main program loop if there is a collision.
-        """
-        self.rect.y = random.randrange(border, wid-border)
-        self.rect.x = random.randrange(border, wid-border)
- 
-    def update(self):
-        """ Called each frame. """
-        # Move block down one pixel
-        self.rect.x += self.velx
-        self.rect.y += self.vely
-        # If block is too far down, reset to top of screen.
-        if self.rect.y > wid-border/2-gotirad:
-            self.vely = -1*abs(self.vely)
-        elif self.rect.y<border/2:
-            self.vely = abs(self.vely)
-        if self.rect.x > wid-border/2-gotirad:
-            self.velx = -1*abs(self.velx)
-        elif self.rect.x<border/2:
-            self.velx = abs(self.velx)
-
-class Striker(Goti):
-    """ The player class derives from Block, but overrides the 'update' 
-    functionality with new a movement function that will move the block 
-    with the mouse. """
-    def update(self):
-        # Get the current mouse position. This returns the position
-        # as a list of two numbers.
-        pos = pygame.mouse.get_pos()
-        # Fetch the x and y out of the list, 
-        # just like we'd fetch letters out of a string.
-        # Set the player object to the mouse location
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
-
-# Define some constants
-BLACK    = (   0,   0,   0)
-BROWN    = (  50,   0,   0)
-WHITE    = ( 255, 255, 255)
-WOOD     = ( 220, 200, 150)
-BLUE     = (   0,   0, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
-PI = 3.141592653
-border = 70
-# Set the width and height of the screen [width, height]
-size = (700, 700)
-wid = size[0]
-strikerrad = 40
-strikerx = 350
-strikery = 350
-pocketrad = 25
-gotirad = 32
-svelx = 3
-svely = 5
-friction = 1
-fps = 60;
 
 pygame.init()
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Carrom")
 
-
-
-
 goti_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
-striker = Striker(BLUE, strikerrad, strikerrad)
+bullet_list = pygame.sprite.Group()
+striker = Striker(BLUE)
 all_sprites_list.add(striker)
+
 for i in range(20):
     # This represents a block
-    goti = Goti(RED, gotirad, gotirad)
+    goti = Goti(RED)
  
     # Set a random location for the block
     goti.rect.x = random.randrange(border, wid-border)
@@ -135,28 +53,52 @@ background = pygame.image.load('data/back.jpg').convert()
 # main screen creation
 score = 0
 screen.blit(background, (0, 0))
-pygame.draw.rect(screen, BROWN, [0, 0, wid, wid], border)
 pygame.display.update()
 
 
+t_list = pygame.sprite.Group()
 while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            bullet = Bullet()
+            bullet.rect.x = striker.rect.x
+            bullet.rect.y = striker.rect.y
+            # Add the bullet to the lists
+            all_sprites_list.add(bullet)
+            bullet_list.add(bullet)
 
     # screen.blit(background, (0, 0))
     screen.fill(WOOD)
 
-    pygame.draw.rect(screen, BROWN, [0, 0, wid, wid], border)
-    # drawCorners()
+    # pygame.draw.rect(screen, BROWN, [0, 0, wid, wid], border)
+    drawBorder()
+    drawCorners()
 
     all_sprites_list.update()
 
+
+    for bullet in bullet_list:
+        # See if it hit a block
+        hit_list = pygame.sprite.spritecollide(bullet, goti_list, True)
+        # For each block hit, remove the bullet and add to the score
+        for goti in hit_list:
+            bullet_list.remove(bullet)
+            all_sprites_list.remove(bullet)
+            score += 1
+            print(score)
+        # Remove the bullet if it flies up off the screen
+        if bullet.rect.y < -10:
+            bullet_list.remove(bullet)
+            all_sprites_list.remove(bullet)
+
+
     for goti in goti_list:
-        t_list = []
+        t_list.empty()
         for t in goti_list:
             if t!=goti:
-                t_list.append(t)
+                t_list.add(t)
 
         temp_hit_list = pygame.sprite.spritecollide(goti, t_list, False)
         for hitGoti in temp_hit_list:
@@ -176,21 +118,8 @@ while not done:
         
     all_sprites_list.draw(screen)
 
-
-
-    # if fps>0:
-    #     strikerx += svelx
-    #     strikery += svely
-    #     # fps -= friction
-    # if strikery > wid-border/2-strikerrad or strikery < border/2+strikerrad:
-    #     svely = svely * -1
-    # if strikerx > wid-border/2-strikerrad or strikerx < border/2+strikerrad:
-    #     svelx = svelx * -1
-    # pygame.draw.circle(screen, BLUE, [strikerx, strikery], strikerrad)
-    # pygame.draw.circle(screen, BLACK, [strikerx, strikery], strikerrad, 2)
-    
     pygame.display.flip()
     # Limit to input frames per second
-    clock.tick(fps)
+    # clock.tick(fps)
 
 pygame.quit()
--- definitions.py
+++ definitions.py
@@ -1,16 +1,100 @@
 import pygame
-def drawCorners():
-    pygame.draw.circle(screen, BLACK, [border, border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [wid-border, border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [border, wid-border], pocketrad)
-    pygame.draw.circle(screen, BLACK, [wid-border, wid-border], pocketrad)
-    return
 
-def drawGotiW(screen, x, y):
-    pygame.draw.circle(screen, WHITE, [x, y], gotirad)
-    pygame.draw.circle(screen, BLACK, [x, y], gotirad, 1)
-    return
+# Define some constants
+BLACK    = (   0,   0,   0)
+LBROWN   = (  70,   0,   0)
+DBROWN   = (  20,   0,   0)
+WHITE    = ( 255, 255, 255)
+WOOD     = ( 220, 200, 150)
+BLUE     = (   0,   0, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+PI = 3.141592653
+border = 80
 
-def drawGotiB(screen, x, y):
-    pygame.draw.circle(screen, (30, 30, 30), [x, y], gotirad)
-    return
+strikerrad = 45
+pocketrad = 30
+gotirad = 32
+gotispeedx = 1.0
+gotispeedy = 2.0
+friction = 1.0
+fps = 100;
+
+# Set the width and height of the screen [width, height]
+size = (700, 700)
+wid = size[0]
+mod = lambda v: sqrt(v[0] * v[0] + v[1] * v[1])
+
+class Goti(pygame.sprite.Sprite):
+    """
+    This class represents the goti.
+    It derives from the "Sprite" class in Pygame.
+    """
+    def __init__(self, color):
+        """ Constructor. Pass in the color of the block,
+        and its x and y position. """
+ 
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+ 
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([gotirad, gotirad])
+        self.image.fill(WOOD)
+        self.image.set_colorkey(WOOD)
+        pygame.draw.ellipse(self.image, color, [0, 0, gotirad, gotirad])
+        pygame.draw.ellipse(self.image, BLACK, [0, 0, gotirad, gotirad], 2)
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values
+        # of rect.x and rect.y
+        self.rect = self.image.get_rect()
+        self.velx = gotispeedx
+        self.vely = gotispeedy
+        self.collided = False
+    
+    def update(self):
+        """ Called each frame. """
+        # Move block down one pixel
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+        # If block is too far down, reset to top of screen.
+        if self.rect.y > wid-border/2-gotirad:
+            self.vely = -1*abs(self.vely)
+        elif self.rect.y<border/2:
+            self.vely = abs(self.vely)
+        if self.rect.x > wid-border/2-gotirad:
+            self.velx = -1*abs(self.velx)
+        elif self.rect.x<border/2:
+            self.velx = abs(self.velx)
+        # self.velx *= 0.9
+        # self.vely *= 0.9
+
+class Striker(Goti):
+    """ The player class derives from Block, but overrides the 'update' 
+    functionality with new a movement function that will move the block 
+    with the mouse. """
+    def update(self):
+        # Get the current mouse position. This returns the position
+        # as a list of two numbers.
+        pos = pygame.mouse.get_pos()
+        # Fetch the x and y out of the list, 
+        # just like we'd fetch letters out of a string.
+        # Set the player object to the mouse location
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+class Bullet(pygame.sprite.Sprite):
+    """ This class represents the bullet . """
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+ 
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(BLACK)
+ 
+        self.rect = self.image.get_rect()
+ 
+    def update(self):
+        """ Move the bullet. """
+        self.rect.y -= 3
