62aca21b37086a8b9ae16fc6288e897e20624661
aseemraj <aseem.mathswiz@gmail.com>
2014-08-05 10:36:39 +0530

    game running

--- carrom.py
+++ carrom.py
@@ -1,9 +1,9 @@
 import pygame, random
-# from pygame.locals import *
+from pygame.locals import *
 from definitions import *
 from math import *
 # from numpy import *
-
+score = [0, 0, 0, 0]
 
 def drawCorners():
     pygame.draw.circle(screen, BLACK, [2*border/3, 2*border/3], pocketrad)
@@ -48,19 +48,76 @@ def drawBorder():
             pygame.draw.rect(screen, DBROWN, [i, i, wid-2*i, wid-2*i], 3)
     return
 
+def inPocket(obj):
+    nearx = min(abs(obj.rect.x-2*border/3), abs(obj.rect.x-wid+2*border/3))
+    neary = min(abs(obj.rect.y-2*border/3), abs(obj.rect.y-wid+2*border/3))
+    if nearx<pocketrad/2 and neary<pocketrad/2:
+        return True
+    return False
+
+def resolveCollision(obj1, obj2):
+    c1 = [obj1.rect.x+obj1.rad, obj1.rect.y+obj1.rad]
+    c2 = [obj2.rect.x+obj2.rad, obj2.rect.y+obj2.rad]
+    distx = abs(c1[0]-c2[0])
+    disty = abs(c1[1]-c2[1])
+    dist = [abs(c1[0]-c2[0]), abs(c1[1]-c2[1])]
+    if mod(dist)==0:
+        costheta = 1
+        sintheta = 0
+    else:
+        costheta = abs(distx)/mod(dist)
+        sintheta = abs(disty)/mod(dist)
+    if mod(dist)<obj1.rad + obj2.rad:
+        diff = obj1.rad + obj2.rad - mod(dist)
+        if obj2.rect.x>=obj1.rect.x:
+            obj2.rect.x += ceil(diff*costheta)
+        else:
+            obj1.rect.x += ceil(diff*costheta)
+        if obj2.rect.y>obj1.rect.y:
+            obj2.rect.y += ceil(diff*costheta)
+        else:
+            obj1.rect.y += ceil(diff*costheta)
+
+    v1x, v1y = obj1.velx, obj1.vely
+    v2x, v2y = obj2.velx, obj2.vely
+    obj1.velx = v1x*sintheta*sintheta + v2x*costheta*costheta + costheta*sintheta*(v2y-v1y)
+    obj2.velx = v1x*costheta*costheta + v2x*sintheta*sintheta + costheta*sintheta*(v1y-v2y)
+    obj1.vely = sintheta*costheta*(v1x-v2x) + v1y*costheta*costheta + v2y*sintheta*sintheta
+    obj2.vely = sintheta*costheta*(v1x-v2x) + v1y*sintheta*sintheta + v2y*costheta*costheta
+    for obj in [obj1, obj2]:
+        if mod([obj.velx, obj.vely])==0:
+            obj.velx = 0
+            obj.vely = 0
+        else:
+            obj.velx = obj.velx - friction * obj.velx / mod([obj.velx, obj.vely])  # friction acts in a direction
+            obj.vely = obj.vely - friction * obj.vely / mod([obj.velx, obj.vely])  # opposite to velocity
+            if abs(obj.velx)<friction:
+                obj.velx = 0
+            if abs(obj.vely)<friction:
+                obj.vely = 0
+
+
 pygame.init()
 screen = pygame.display.set_mode(scrsize)
 pygame.display.set_caption("Carrom")
 
 goti_list = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
-bullet_list = pygame.sprite.Group()
-striker = Striker(BLUE)
+striker = Striker()
 all_sprites_list.add(striker)
 
 for i in range(10):
     # This represents a block
-    goti = Goti(DGRAY)
+    goti = Goti(BLACK)
+    # Set a random location for the block
+    goti.rect.x = random.randrange(border, wid-border)
+    goti.rect.y = random.randrange(border, wid-border)
+    # Add the goti to the list of objects
+    goti_list.add(goti)
+    all_sprites_list.add(goti)
+for i in range(10):
+    # This represents a block
+    goti = Goti(WHITE)
     # Set a random location for the block
     goti.rect.x = random.randrange(border, wid-border)
     goti.rect.y = random.randrange(border, wid-border)
@@ -68,31 +125,32 @@ for i in range(10):
     goti_list.add(goti)
     all_sprites_list.add(goti)
 
-
-
 done = False    # loop till close button clicked
 clock = pygame.time.Clock()
 
 background = pygame.image.load('data/back.jpg').convert()
 
 # main screen creation
-score = 0
 screen.blit(background, (0, 0))
 pygame.display.update()
 
 
-t_list = pygame.sprite.Group()
 while not done:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            bullet = Bullet()
-            bullet.rect.x = striker.rect.x
-            bullet.rect.y = striker.rect.y
-            # Add the bullet to the lists
-            all_sprites_list.add(bullet)
-            bullet_list.add(bullet)
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
+            if striker.state==0:
+                striker.state = 1
+            elif striker.state==1:
+                striker.state = 2
+                cstriker = [striker.rect.x+strikerrad/2, striker.rect.y+strikerrad/2]
+                pos = pygame.mouse.get_pos()
+                striker.velx = (cstriker[0] - pos[0])/4
+                striker.vely = (cstriker[1] - pos[1])/4
+
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
+            striker.state = 0
 
     # screen.blit(background, (0, 0))
     screen.fill(WOOD)
@@ -103,39 +161,67 @@ while not done:
 
     all_sprites_list.update()
 
-
-    for bullet in bullet_list:
-        # See if it hit a block
-        hit_list = pygame.sprite.spritecollide(bullet, goti_list, True)
-        # For each block hit, remove the bullet and add to the score
-        for goti in hit_list:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
-            score += 1
-            print(score)
-        # Remove the bullet if it flies up off the screen
-        if bullet.rect.y < -10:
-            bullet_list.remove(bullet)
-            all_sprites_list.remove(bullet)
-
-
-    for goti in goti_list:
-        hitObj = pygame.sprite.spritecollideany(goti, goti_list)
-        if hitObj is not None:
-            c1 = [hitObj.rect.x+gotirad/2, hitObj.rect.y+gotirad/2]
-            c2 = [goti.rect.x+gotirad/2, goti.rect.y+gotirad/2]
-
-            hitObj.velx, goti.velx = goti.velx, hitObj.velx
-            hitObj.vely, goti.vely = goti.vely, hitObj.vely
-
-    # striker hits a goti
-    goti_hit_list = pygame.sprite.spritecollide(striker, goti_list, False)
-    for goti in goti_hit_list:
-        goti.velx = -1*goti.velx
-        goti.vely = -1*goti.vely
+    for goti1 in goti_list:
+        for goti2 in goti_list:
+            if goti1 is not goti2 and pygame.sprite.collide_circle(goti1, goti2) and not goti1.collided and not goti2.collided:
+                resolveCollision(goti1, goti2)
+                goti1.collided, goti2.collided = True, True
 
     cstriker = [striker.rect.x+strikerrad/2, striker.rect.y+strikerrad/2]
+
+    # striker is in angle select mode
+    if striker.state==1:
+        pos = pygame.mouse.get_pos()
+        if pos[0]>border+strikerrad+gotirad and pos[0]<wid-(border+strikerrad+gotirad) and pos[1]>border+strikerrad+gotirad and pos[1]<wid-(border+strikerrad+gotirad):
+            pygame.draw.line(screen, RED,(cstriker[0],cstriker[1]),(pos[0],pos[1]),5)
+        else:
+            pygame.draw.line(screen, GREEN,(cstriker[0],cstriker[1]),(pos[0],pos[1]),5)
+
+    foul = False
+    # striker hits a goti
+    if striker.state==2:
+        for goti in goti_list:
+            if pygame.sprite.collide_circle(goti, striker):
+                resolveCollision(striker, goti)
+            if inPocket(striker):
+                foul = True
+                # goti.velx = -1*goti.velx
+                # goti.vely = -1*goti.vely
+    
+    chplayer = True
+    for disk in all_sprites_list:
+        if disk.velx!=0 or disk.vely!=0:
+            chplayer = False
+            break
+
+    chance = False
+    for goti in goti_list:
+        goti.collided = False
+        if inPocket(goti):
+            if goti.isWhite:
+                score[striker.player] += 20
+            else:
+                score[striker.player] += 10
+            chance = True
+            goti_list.remove(goti)
+            all_sprites_list.remove(goti)
+
+    if chplayer and striker.state==2:
+        striker.state = 0
+        if foul:
+            print "Foul by player ", striker.player
+            score[striker.player] -= 10
+            fgoti = Goti(BLACK)
+            fgoti.rect.x = wid/2-gotirad/2
+            fgoti.rect.y = wid/2-gotirad/2
+        if not chance:
+            striker.player = (striker.player+1)%4
+        for e in range(4):
+            print "Player ", e+1, ": ", score[e]
+        print "\n"
+
     all_sprites_list.draw(screen)
+
 
     pygame.display.flip()
     # Limit to input frames per second
--- definitions.py
+++ definitions.py
@@ -1,9 +1,10 @@
 import pygame
+from math import *
 
 # Define some constants
 BLACK    = (   0,   0,   0)
-LBROWN   = (  70,   0,   0)
-DBROWN   = (  20,   0,   0)
+LBROWN   = (  80,  30,   0)
+DBROWN   = (  50,  20,   0)
 DGRAY    = (  30,  30,  30)
 WHITE    = ( 255, 255, 255)
 WOOD     = ( 220, 200, 150)
@@ -11,18 +12,20 @@ BLUE     = (   0,   0, 255)
 GREEN    = (   0, 255,   0)
 RED      = ( 255,   0,   0)
 PI = 3.141592653
-border = 80
+LEFT = 1
+RIGHT = 3
+border = 70
 
-strikerrad = 45
+strikerrad = 40
 pocketrad = 30
-gotirad = 32
-gotispeedx = 1.0
-gotispeedy = 2.0
-friction = 1.0
-fps = 100;
+gotirad = 26
+gotispeedx = 0
+gotispeedy = 0
+friction = 0.1
+fps = 70;
 
 # Set the width and height of the screen [width, height]
-scrsize = (700, 700)
+scrsize = (720, 720)
 wid = scrsize[0]
 mod = lambda v: sqrt(v[0] * v[0] + v[1] * v[1])
 
@@ -43,17 +46,25 @@ class Goti(pygame.sprite.Sprite):
         self.image = pygame.Surface([gotirad, gotirad])
         self.image.fill(WOOD)
         self.image.set_colorkey(WOOD)
-        pygame.draw.ellipse(self.image, color, [0, 0, gotirad, gotirad])
-        pygame.draw.ellipse(self.image, BLACK, [0, 0, gotirad, gotirad], 2)
+        if color==BLACK:
+            self.isWhite = False
+            for i in range(gotirad/2):
+                pygame.draw.ellipse(self.image, (5*i, 5*i, 5*i), [i, i, gotirad-2*i, gotirad-2*i])
+                pygame.draw.ellipse(self.image, BLACK, [0, 0, gotirad, gotirad], 2)
+        elif color==WHITE:
+            self.isWhite = True
+            for i in range(gotirad/2):
+                pygame.draw.ellipse(self.image, (255-5*i, 255-5*i, 255-5*i), [i, i, gotirad-2*i, gotirad-2*i])
+                pygame.draw.ellipse(self.image, WHITE, [0, 0, gotirad, gotirad], 2)
         # Fetch the rectangle object that has the dimensions of the image
         # image.
         # Update the position of this object by setting the values
         # of rect.x and rect.y
         self.rect = self.image.get_rect()
+        self.rad = gotirad/2
         self.velx = gotispeedx
         self.vely = gotispeedy
         self.collided = False
-        self.radius = gotirad/2
 
     def update(self):
         """ Called each frame. """
@@ -62,19 +73,33 @@ class Goti(pygame.sprite.Sprite):
         self.rect.y += self.vely
         # If block is too far down, reset to top of screen.
         if self.rect.y > wid-border/2-gotirad:
+            self.rect.y = wid - border/2 - gotirad - 1
             self.vely = -1*abs(self.vely)
         elif self.rect.y<border/2:
+            self.rect.y = border/2 + 1
             self.vely = abs(self.vely)
         if self.rect.x > wid-border/2-gotirad:
+            self.rect.x = wid - border/2 - gotirad - 1
             self.velx = -1*abs(self.velx)
         elif self.rect.x<border/2:
+            self.rect.x = border/2 + 1
             self.velx = abs(self.velx)
+        if mod([self.velx, self.vely])==0:
+            self.velx = 0
+            self.vely = 0
+        else:
+            self.velx = self.velx - friction * self.velx / mod([self.velx, self.vely])  # friction acts in a direction
+            self.vely = self.vely - friction * self.vely / mod([self.velx, self.vely])  # opposite to velocity
+            if abs(self.velx)<friction:
+                self.velx = 0
+            if abs(self.vely)<friction:
+                self.vely = 0
 
 class Striker(Goti):
     """ The player class derives from Block, but overrides the 'update' 
     functionality with new a movement function that will move the block 
     with the mouse. """
-    def __init__(self, color):
+    def __init__(self):
         """ Constructor. Pass in the color of the block,
         and its x and y position. """
  
@@ -86,40 +111,70 @@ class Striker(Goti):
         self.image = pygame.Surface([strikerrad, strikerrad])
         self.image.fill(WOOD)
         self.image.set_colorkey(WOOD)
-        pygame.draw.ellipse(self.image, color, [0, 0, strikerrad, strikerrad])
+        for i in range(strikerrad/2):
+            pygame.draw.ellipse(self.image, (5*i, 5*i, 100+5*i), [i, i, strikerrad-2*i, strikerrad-2*i])
+        # pygame.draw.ellipse(self.image, color, [0, 0, strikerrad, strikerrad])
         pygame.draw.ellipse(self.image, BLACK, [0, 0, strikerrad, strikerrad], 2)
         # Fetch the rectangle object that has the dimensions of the image
         # image.
         # Update the position of this object by setting the values
         # of rect.x and rect.y
+        self.velx = 0
+        self.vely = 0
         self.rect = self.image.get_rect()
-        self.velx = gotispeedx
-        self.vely = gotispeedy
+        self.rad = strikerrad/2
         self.collided = False
-        self.radius = strikerrad/2
+        self.state = 0
+        self.player = 0
 
     def update(self):
-        # Get the current mouse position. This returns the position
-        # as a list of two numbers.
-        pos = pygame.mouse.get_pos()
-        # Fetch the x and y out of the list, 
-        # just like we'd fetch letters out of a string.
-        # Set the player object to the mouse location
-        self.rect.x = pos[0]
-        self.rect.y = pos[1]
+        # User is still placing the striker
+        if self.state==0:
+            pos = pygame.mouse.get_pos()
+            if self.player==0 or self.player==2:
+                self.rect.x = pos[0]-strikerrad/2
+                if self.player==0:
+                    self.rect.y = wid-pocketrad-border-gotirad/2-strikerrad/2
+                else:
+                    self.rect.y = pocketrad+border+gotirad/2-strikerrad/2
+                if pos[0]>wid-(strikerrad/2+pocketrad+border+gotirad):
+                    self.rect.x = wid-(strikerrad+pocketrad+border+gotirad)
+                elif pos[0]<pocketrad+border+gotirad+strikerrad/2:
+                    self.rect.x = pocketrad+border+gotirad
+            else:
+                self.rect.y = pos[1]-strikerrad/2
+                if self.player==1:
+                    self.rect.x = wid-pocketrad-border-gotirad/2-strikerrad/2
+                else:
+                    self.rect.x = pocketrad+border+gotirad/2-strikerrad/2
+                if pos[1]>wid-(strikerrad/2+pocketrad+border+gotirad):
+                    self.rect.y = wid-(strikerrad+pocketrad+border+gotirad)
+                elif pos[1]<pocketrad+border+gotirad+strikerrad/2:
+                    self.rect.y = pocketrad+border+gotirad
 
-class Bullet(pygame.sprite.Sprite):
-    """ This class represents the bullet . """
-    def __init__(self):
-        # Call the parent class (Sprite) constructor
-        pygame.sprite.Sprite.__init__(self)
- 
-        self.image = pygame.Surface([4, 10])
-        self.image.fill(BLACK)
- 
-        self.rect = self.image.get_rect()
- 
-    def update(self):
-        """ Move the bullet. """
-        self.rect.y -= 3
-
+        # User launched the striker
+        elif self.state==2:
+            self.rect.x += self.velx
+            self.rect.y += self.vely
+            if self.rect.y > wid-border/2-gotirad:
+                self.rect.y = wid - border/2 - gotirad - 1
+                self.vely = -1*abs(self.vely)
+            elif self.rect.y<border/2:
+                self.rect.y = border/2 + 1
+                self.vely = abs(self.vely)
+            if self.rect.x > wid-border/2-gotirad:
+                self.rect.x = wid - border/2 - gotirad - 1
+                self.velx = -1*abs(self.velx)
+            elif self.rect.x<border/2:
+                self.rect.x = border/2 + 1
+                self.velx = abs(self.velx)
+            if mod([self.velx, self.vely])==0:
+                self.velx = 0
+                self.vely = 0
+            else:
+                self.velx = self.velx - friction * self.velx / mod([self.velx, self.vely])  # friction acts in a direction
+                self.vely = self.vely - friction * self.vely / mod([self.velx, self.vely])  # opposite to velocity
+                if abs(self.velx)<friction:
+                    self.velx = 0
+                if abs(self.vely)<friction:
+                    self.vely = 0
